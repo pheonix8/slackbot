9bbc0e7d9fa87a3d4e863cc4e7670223e57d7473
pheonix8 <leo.oetterli@gmx.ch>
2020-10-21 17:03:14 +0200

    Update requirements.txt and slackbot_settings.py

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,4 +1,9 @@
+# Slack Tokens
 SLACK_SIGNING_SECRET = "secret"
 SLACK_BOT_TOKEN = "bottoken"
 SLACK_VERIFICATION_TOKEN = "verificationtoken"
 SLACK_TEAM_ID = "teamid"
+
+# Unleashed Tokens
+API_KEY = "key"
+API_ID = "id"
